8484e4a953fcce0f0a0d27f7fe0dfdd1dba3c1b5
Erika Tharp <eems0926@gmail.com>
2020-09-03 17:09:18 -0700

    Initial commit

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    titleText: "",
+    content: ""
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAddNote(note);
+    setNote({
+      titleText: "",
+      content: ""
+    });
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      <form>
+        <input
+          name="titleText"
+          onChange={handleChange}
+          value={note.titleText}
+          placeholder="Title"
+        />
+        <textarea
+          name="content"
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Keep something inspiring here..."
+          rows="3"
+        />
+        <button onClick={submitNote}>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1 className="title">Sticky Notes</h1>
+      <h4 className="title">なんでもノート</h4>
+    </header>
+  );
+}
+
+export default Header;
